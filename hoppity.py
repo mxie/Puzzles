c7c895eb856e78e5689f70f10373b5d7a9b2c434
Melissa Xie <mlxie8289@gmail.com>
2011-02-06 15:38:45 -0500

    nearly done! need for loop

--- hoppity.py
+++ hoppity.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python
+def get_num(s):
+	import re
+	m = re.match(r"\s*(\d+)\s*", s)
+	return m.group()
 
+
+def make_hops(num):
+	output = open('result.txt', 'wa')
+
+#for loop here
+
+	output.close()
+
+if __name__ == "__main__":
+	import sys
+
+	input = open(sys.argv[1])
+	num = get_num(input.read())
+	input.close()
+
+	make_hops(num)
