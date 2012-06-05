db06121a86a17347843ca1b50cbed72f25296ca3
Melissa Xie <mlxie8289@gmail.com>
2012-06-04 20:41:44 -0400

    Eliminates int-string conversions, fixes the processing for repeated numerals

--- roman.py
+++ roman.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python
 import sys
 
-roman_nums = dict({ "1":"I", "5":"V", "10":"X", "50":"L", "100":"C", "500":"D", "1000":"M" })
-spec_nums = dict({ "4":"IV", "9":"IX", "40":"XL", "90":"XC", "400":"CD", "900":"CM" })
+base_roman = { 1:"I", 5:"V", 10:"X", 50:"L", 100:"C", 500:"D", 1000:"M" }
+special_roman = { 4:"IV", 9:"IX", 40:"XL", 90:"XC", 400:"CD", 900:"CM" }
 
 def get_roman(num):
     result = []
-    place = 1;
-    num = num[::-1]
-    for i in num:
-        i_num = int(i)
-        actual = str(i_num*place)
-        if (i in roman_nums):
-            result.append(roman_nums[actual])
-        elif (i in spec_nums):
-            result.append(spec_nums[actual])
-        else:
-            result.append(parse_num(i, place))
-        place *= 10
-        num.replace(i, '', 1)
-    
+    num = num[::-1]         # reverse the digits
+    for i in range(len(num)):
+        place = 10 ** i     # initially ones place, then tens, etc.
+        actual_num = int(num[i]) * place
+        if actual_num in base_roman:        # 1, 5, 10, 50, ...
+            result.append(base_roman[actual_num])
+        elif actual_num in special_roman:   # 4, 9, 40, ...
+            result.append(special_roman[actual_num])
+        else:                               # any other number
+            result.append(parse_num(actual_num, place))
+    # join the roman rep of each digit in reverse order of the list
     return ''.join(result[::-1])
 
 def parse_num(num, place):
-    i = int(num)
-    n = i if (i < 4) else i-5
-    result = '' if (i < 4) else roman_nums["5"] 
-    while (n > 0):
-        result += roman_nums[str(place)]
-        n -= 1
-
+    four_mult = 4*place
+    five_mult = 5*place
+    n = num if num < four_mult else num-five_mult
+    # start off with the roman rep of the five multiple if greater
+    result = '' if num < four_mult else base_roman[five_mult] 
+    while n > 0:
+        result += base_roman[place]
+        n -= place
     return result
 
 def main():
